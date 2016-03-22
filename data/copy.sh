88d3ec8cbed3e17a93fc68b37460e8b023bba01d
Zachary Leung <zacharyleung@alum.mit.edu>
2016-03-22 23:29:43 +0800

    added lead time data files

---
+++ data/copy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp ~/git/zambia-paper/data/clean/facility-accessibility.csv .
+cp ~/git/zambia-paper/data/clean/facility-deliveries.csv .
+cp ~/git/zambia-paper/data/clean/facility-reports.csv .
+cp ~/git/zambia-paper/data/clean/msl-deliveries.csv .
+cp ~/git/zambia-paper/data/compute/lead-times.csv facility-lead-times.csv
+
+
